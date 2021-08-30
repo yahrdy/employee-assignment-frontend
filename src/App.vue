753692f433a34c99a2a0096816eccbc2dfeeee8a
Yeasir Arafat <yeasir.arafat@tripegate.com>
2021-08-30 22:03:19 +0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -68,7 +68,7 @@
           </v-list-item>
           <v-list-item :to="{name:'AllDepartments'}">
             <v-list-item-icon>
-              <v-icon>mdi-information-outline</v-icon>
+              <v-icon>mdi-theater</v-icon>
             </v-list-item-icon>
             <v-list-item-title>
               Departments
@@ -76,7 +76,7 @@
           </v-list-item>
           <v-list-item :to="{name:'AllEmployees'}">
             <v-list-item-icon>
-              <v-icon>mdi-information-outline</v-icon>
+              <v-icon>mdi-account-multiple</v-icon>
             </v-list-item-icon>
             <v-list-item-title>
               Employees
